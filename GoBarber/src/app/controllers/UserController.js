10686dc032cc94e87c9d3ea19fce75400fb8adce
Neto6391 <neto6391@gmail.com>
2020-02-02 22:37:46 -0300

    Implements User Creation

---
+++ GoBarber/src/app/controllers/UserController.js
@@ -0,0 +1,33 @@
+import User from '../models/User';
+
+class UserController {
+    async store(req, res) {
+        const userExists = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+
+        if (userExists) {
+            return res.status(400).json({
+                error: 'User already exists',
+            });
+        }
+
+        if (req.body.hasOwnProperty('password_hash')) {
+            return res.status(400).json({
+                error: 'Password is Required',
+            });
+        }
+
+        const { id, name, email, provider } = await User.create(req.body);
+        return res.json({
+            id,
+            name,
+            email,
+            provider,
+        });
+    }
+}
+
+export default new UserController();
