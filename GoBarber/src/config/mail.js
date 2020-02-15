9c01abd538cd55227409f41ea474b278664d46d2
Neto6391 <neto6391@gmail.com>
2020-02-15 00:52:10 -0300

    ADD email feature

---
+++ GoBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: 'a1f93e1291550e',
+        pass: '5764981e2bb95b',
+    },
+    default: {
+        from: 'Equipe <noreply@gobarber.com>',
+    },
+};
