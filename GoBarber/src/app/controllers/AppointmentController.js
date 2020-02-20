5d3f6a738a8234f279272e122801f1cff2eaa884
Neto6391 <neto6391@gmail.com>
2020-02-20 00:10:54 -0300

    ADD Queue Tasks in BackgroundJobs

--- GoBarber/src/app/controllers/AppointmentController.js
+++ GoBarber/src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import File from '../models/File';
 import Notification from '../schemas/Notification';
 import * as yup from 'yup';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
     async index(req, res) {
@@ -148,7 +149,7 @@ class AppointmentController {
                 },
             ],
         });
-        console.log(appointment);
+
         if (appointment.user_id !== req.userId) {
             return res.status(401).json({
                 error:
@@ -170,21 +171,9 @@ class AppointmentController {
         appointment.canceled_at = new Date();
 
         await appointment.save();
-        await Mail.sendMail({
-            to: `${appointment.provider.name} <${appointment.provider.email}>`,
-            subject: 'Agendamento cancelado',
-            template: 'cancellation',
-            context: {
-                provider: appointment.provider.name,
-                user: appointment.user.name,
-                date: format(
-                    appointment.date,
-                    "'dia' dd 'de' MMM', às 'H:mm'h'",
-                    {
-                        locale: pt,
-                    }
-                ),
-            },
+
+        await Queue.add(CancellationMail.key, {
+            appointment,
         });
 
         return res.json(appointment);
